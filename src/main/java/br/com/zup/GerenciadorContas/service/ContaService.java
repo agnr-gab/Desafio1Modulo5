0ed08322eab39e6ef601729b5bb701efffaa76c5
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 16:47:53 -0300

    Criar Classe Conta Service

--- src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
+++ src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
@@ -1,2 +1,15 @@
-package br.com.zup.GerenciadorContas.service;public class ContaService {
+package br.com.zup.GerenciadorContas.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContaService {
+
+    /*
+salvarConta()
+atualizarConta()
+deletarConta()
+exibirConta()
+exibirTodasAsContas()
+     */
 }
