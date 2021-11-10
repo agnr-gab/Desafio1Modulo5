6f9999fdeff76a3dcf1dca10c6676561a8e55c1a
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 16:49:34 -0300

    Criar Classe Conta Controller

--- src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
@@ -1,2 +1,7 @@
-package br.com.zup.GerenciadorContas.controller;public class ContaController {
+package br.com.zup.GerenciadorContas.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContaController {
 }
