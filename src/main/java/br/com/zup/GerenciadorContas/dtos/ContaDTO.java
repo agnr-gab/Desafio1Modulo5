6a58faf3e0fc78ee6b0627925230a649452c6b72
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 17:44:56 -0300

    Inserir Atributos na Classe Conta DTO

--- src/main/java/br/com/zup/GerenciadorContas/dtos/ContaDTO.java
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/ContaDTO.java
@@ -1,4 +1,18 @@
 package br.com.zup.GerenciadorContas.dtos;
 
+import br.com.zup.GerenciadorContas.enums.Tipo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContaDTO {
+    private String nome;
+    private double valor;
+    private Tipo tipo;
+    private LocalDate dataDeVencimento;
 }
