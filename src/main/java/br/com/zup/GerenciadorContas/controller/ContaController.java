673ef59e36855e97a16f31593cac60b8a6038138
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 17:57:35 -0300

    Inserir injeção de dependencia Service e ModelMapper

--- src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
@@ -1,7 +1,16 @@
 package br.com.zup.GerenciadorContas.controller;
 
+import br.com.zup.GerenciadorContas.service.ContaService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping ("/contas")
 public class ContaController {
+    @Autowired
+    private ContaService contaService;
+    @Autowired
+    private ModelMapper modelMapper;
 }
