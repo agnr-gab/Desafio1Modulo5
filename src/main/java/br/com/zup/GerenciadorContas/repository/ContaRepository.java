5b4166809512fac6c5aaf151b083e6986dcb208f
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 19:37:04 -0300

    Corrigir tipos

--- src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
@@ -1,8 +1,10 @@
 package br.com.zup.GerenciadorContas.repository;
 
+import br.com.zup.GerenciadorContas.model.Conta;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContaRepository extends CrudRepository {
+public interface ContaRepository extends CrudRepository <Conta, String>{
+
 }
