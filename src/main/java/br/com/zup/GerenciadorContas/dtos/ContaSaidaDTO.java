b0c09096d137c73a6a978380db05f718ccde507b
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 17:43:58 -0300

    Inserir Atributos na Classe Conta Saida DTO

--- src/main/java/br/com/zup/GerenciadorContas/dtos/ContaSaidaDTO.java
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/ContaSaidaDTO.java
@@ -1,4 +1,22 @@
 package br.com.zup.GerenciadorContas.dtos;
 
+import br.com.zup.GerenciadorContas.enums.Status;
+import br.com.zup.GerenciadorContas.enums.Tipo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContaSaidaDTO {
+    private int id;
+    private String nome;
+    private Tipo tipo;
+    private LocalDateTime dataDeVencimento;
+    private LocalDate dataDePagamento;
+    private Status status;
+
 }
