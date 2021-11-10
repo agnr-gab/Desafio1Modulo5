69575a8140cfc6492976b86701929703516a741a
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 16:42:42 -0300

    Criar Classe Enum Tipo

--- src/main/java/br/com/zup/GerenciadorContas/enums/Tipo.java
+++ src/main/java/br/com/zup/GerenciadorContas/enums/Tipo.java
@@ -1,2 +1,23 @@
-package br.com.zup.GerenciadorContas.enums;public enum Tipo {
+package br.com.zup.GerenciadorContas.enums;
+
+public enum Tipo {
+    LUZ,
+    AGUA,
+    COMIDA,
+    LAZER,
+    OUTROS;
+
+    private String tipoConta;
+
+    Tipo() {
+    }
+
+    Tipo(String tipoConta) {
+
+        this.tipoConta = tipoConta;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
 }
