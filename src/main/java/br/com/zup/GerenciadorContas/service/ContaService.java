49b04dc3fcb26cd8dd211e5723d40595c05794a6
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 19:36:20 -0300

    Criar Metodo Salvar Conta

--- src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
+++ src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
@@ -1,9 +1,22 @@
 package br.com.zup.GerenciadorContas.service;
 
+import br.com.zup.GerenciadorContas.model.Conta;
+import br.com.zup.GerenciadorContas.repository.ContaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Service
 public class ContaService {
+    @Autowired
+    private ContaRepository contaRepository;
+
+    public Conta salvarConta(Conta conta){
+        conta.setDataDeVencimento(LocalDate.now());
+        return contaRepository.save(conta);
+    }
+
 
     /*
 salvarConta()
