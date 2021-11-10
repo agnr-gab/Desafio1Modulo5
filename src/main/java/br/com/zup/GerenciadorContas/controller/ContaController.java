100e541554d3246f27a7d0ae03497f9035a5ee43
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 20:08:50 -0300

    Inserir metodo de cadastrarConta

--- src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
@@ -1,10 +1,12 @@
 package br.com.zup.GerenciadorContas.controller;
 
+import br.com.zup.GerenciadorContas.dtos.ContaDTO;
+import br.com.zup.GerenciadorContas.model.Conta;
 import br.com.zup.GerenciadorContas.service.ContaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping ("/contas")
@@ -13,4 +15,14 @@ public class ContaController {
     private ContaService contaService;
     @Autowired
     private ModelMapper modelMapper;
+
+    @PostMapping
+    @ResponseStatus (HttpStatus.CREATED)
+    public void cadastrarConta(@RequestBody ContaDTO contaDTO) {
+        Conta conta = modelMapper.map(contaDTO, Conta.class);
+        contaService.salvarConta(conta);
+    }
+
+
+
 }
