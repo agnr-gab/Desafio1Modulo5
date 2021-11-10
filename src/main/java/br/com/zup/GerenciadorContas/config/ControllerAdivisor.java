ad8d477320d47d1cc41659f1337f515fa89f13ad
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-10 18:08:26 -0300

    Criar Classe Controller Advisor

---
+++ src/main/java/br/com/zup/GerenciadorContas/config/ControllerAdivisor.java
@@ -0,0 +1,10 @@
+package br.com.zup.GerenciadorContas.config;
+
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdivisor {
+
+}
