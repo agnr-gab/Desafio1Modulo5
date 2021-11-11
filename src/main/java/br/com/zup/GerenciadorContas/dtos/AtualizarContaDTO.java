cf8253d4b12bacf167e50f3128f219d51fca62e0
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-11 15:07:36 -0300

    Inserir anotações na classe AtualizarContaDTO

--- src/main/java/br/com/zup/GerenciadorContas/dtos/AtualizarContaDTO.java
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/AtualizarContaDTO.java
@@ -1,7 +1,13 @@
 package br.com.zup.GerenciadorContas.dtos;
 
 import br.com.zup.GerenciadorContas.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class AtualizarContaDTO {
     private Status status;
 }
