1aeb4a1b30d30271509d771655beacbde9294c4d
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-11 14:40:52 -0300

    Alterar tipo do atributo Wrapper

--- src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContaRepository extends CrudRepository <Conta, String>{
+public interface ContaRepository extends CrudRepository <Conta, Integer>{
 
 }
