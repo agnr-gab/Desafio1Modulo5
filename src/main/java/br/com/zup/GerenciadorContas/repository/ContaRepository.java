a07cf5185f96707ad52223fe84ecbc0087129684
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-11 15:47:49 -0300

    Inserir Método de Busca

--- src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
@@ -1,10 +1,16 @@
 package br.com.zup.GerenciadorContas.repository;
 
+import br.com.zup.GerenciadorContas.enums.Status;
 import br.com.zup.GerenciadorContas.model.Conta;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface ContaRepository extends CrudRepository <Conta, Integer>{
+    List<Conta> findAllById(int id);
+    List<Conta> findAllByNome(String nome);
+    List<Conta> findAllByValor(double valor);
+    List<Conta> findAllByStatus(Status status);
 
 }
