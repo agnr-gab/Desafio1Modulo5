a4d088d906e1a211a8422aff1e3d269fd822bb72
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-11 16:21:32 -0300

    Criar metodo de exibir todas as contas

--- src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
+++ src/main/java/br/com/zup/GerenciadorContas/service/ContaService.java
@@ -1,20 +1,49 @@
 package br.com.zup.GerenciadorContas.service;
 
+import br.com.zup.GerenciadorContas.dtos.ContaDTO;
+import br.com.zup.GerenciadorContas.dtos.ContaSaidaDTO;
+import br.com.zup.GerenciadorContas.enums.Status;
 import br.com.zup.GerenciadorContas.model.Conta;
 import br.com.zup.GerenciadorContas.repository.ContaRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class ContaService {
     @Autowired
     private ContaRepository contaRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public Conta salvarConta(Conta conta) {
         conta.setDataDeVencimento(LocalDate.now());
         return contaRepository.save(conta);
+    }
+
+    public void verificarStatus(ContaDTO contaDTO, ContaSaidaDTO contaSaidaDTO) {
+        if (contaDTO.getDataDeVencimento().isBefore(LocalDate.now())) {
+            contaSaidaDTO.setStatus(Status.VENCIDA);
+        } else {
+            contaSaidaDTO.setStatus(Status.AGUARDANDO);
+        }
+    }
+
+    public List<Conta> exibirTodasAsContas(Integer id, String nome, Double valor, Status status) {
+        if (id != null) {
+            contaRepository.findAllById(id);
+        } else if (nome != null) {
+            contaRepository.findAllByNome(nome);
+        } else if (valor != null) {
+            contaRepository.findAllByValor(valor);
+        } else if (status != null) {
+            contaRepository.findAllByStatus(status);
+        }
+        List<Conta> listaDeContas = (List<Conta>) contaRepository.findAll();
+        return listaDeContas;
     }
     /*public Conta atualizarConta(){
 
