f7055c712c1c91465864ab6598458fedcb0b21b8
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-11 16:22:36 -0300

    Inserir metodo get de exibir lista

--- src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
+++ src/main/java/br/com/zup/GerenciadorContas/controller/ContaController.java
@@ -1,6 +1,8 @@
 package br.com.zup.GerenciadorContas.controller;
 
+import br.com.zup.GerenciadorContas.dtos.AtualizarContaDTO;
 import br.com.zup.GerenciadorContas.dtos.ContaDTO;
+import br.com.zup.GerenciadorContas.dtos.ContaExibicaoDTO;
 import br.com.zup.GerenciadorContas.model.Conta;
 import br.com.zup.GerenciadorContas.service.ContaService;
 import org.modelmapper.ModelMapper;
@@ -8,8 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-@RequestMapping ("/contas")
+@RequestMapping("/contas")
 public class ContaController {
     @Autowired
     private ContaService contaService;
@@ -17,12 +22,21 @@ public class ContaController {
     private ModelMapper modelMapper;
 
     @PostMapping
-    @ResponseStatus (HttpStatus.CREATED)
+    @ResponseStatus(HttpStatus.CREATED)
     public void cadastrarConta(@RequestBody ContaDTO contaDTO) {
         Conta conta = modelMapper.map(contaDTO, Conta.class);
         contaService.salvarConta(conta);
     }
 
+    @GetMapping
+    public List<ContaExibicaoDTO> exibirLista() {
+        List<ContaExibicaoDTO> listaExibicao = new ArrayList<>();
+        for (Conta referencia : contaService.exibirTodasAsContas()) {
+            ContaExibicaoDTO contaExibicaoDTO = modelMapper.map(referencia, ContaExibicaoDTO.class);
+            listaExibicao.add(contaExibicaoDTO);
+        }
+        return listaExibicao;
+    }
 
 
 }
