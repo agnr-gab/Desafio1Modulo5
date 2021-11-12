4ad57d804cef9333918c51179180269581610047
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-12 20:50:08 -0300

    Inserir pesquisa por valor aproximado @Query

--- src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
+++ src/main/java/br/com/zup/GerenciadorContas/repository/ContaRepository.java
@@ -3,16 +3,20 @@ package br.com.zup.GerenciadorContas.repository;
 import br.com.zup.GerenciadorContas.enums.Status;
 import br.com.zup.GerenciadorContas.enums.Tipo;
 import br.com.zup.GerenciadorContas.model.Conta;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
 public interface ContaRepository extends CrudRepository <Conta, Integer>{
+
     List<Conta> findAllById(int id);
     List<Conta> findAllByNome(String nome);
-    List<Conta> findAllByValor(double valor);
     List<Conta> findAllByStatus(Status status);
     List<Conta> findAllByTipo(Tipo tipo);
+
+    @Query(value = "SELECT * FROM contas WHERE valor BETWEEN :valor-10 AND :valor+10", nativeQuery = true)
+    List<Conta> findAllByValor(double valor);
 
 }
