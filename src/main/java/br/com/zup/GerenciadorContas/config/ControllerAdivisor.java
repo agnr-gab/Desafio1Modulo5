068989a9e2dc608cfd1356485425dfb67a3fd67f
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-12 00:50:26 -0300

    Inserir exceções

--- src/main/java/br/com/zup/GerenciadorContas/config/ControllerAdivisor.java
+++ src/main/java/br/com/zup/GerenciadorContas/config/ControllerAdivisor.java
@@ -1,10 +1,19 @@
 package br.com.zup.GerenciadorContas.config;
 
+import br.com.zup.GerenciadorContas.exceptions.ContaNaoEncontradaException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ControllerAdivisor {
+
+    @ExceptionHandler(ContaNaoEncontradaException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public MensagemDeErro manipularExececaoDeContaNaoEncontrada(ContaNaoEncontradaException exception) {
+        return new MensagemDeErro(exception.getMessage());
+    }
 
 }
