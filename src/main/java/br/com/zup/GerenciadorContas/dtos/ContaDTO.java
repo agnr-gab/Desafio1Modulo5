cf58288082addd830affbbf2550297b6cbdd6ecd
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-12 17:40:01 -0300

    Alterar anotações de validações

--- src/main/java/br/com/zup/GerenciadorContas/dtos/ContaDTO.java
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/ContaDTO.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
@@ -15,11 +15,13 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContaDTO {
+    //@NotEmpty (message = "Campo obrigatório. Por favor, digite um nome entre 2 a 20 caracteres")
     @Size(min = 2, max = 20, message = "Campo obrigatório. Por favor, digite um nome entre 2 a 20 caracteres")
     private String nome;
-    @NotNull(message = "Campo obrigatório. Por favor, digite o valor da conta")
+    //@NotNull(message = "Campo obrigatório. Por favor, digite o valor da conta")
+    @Min(value = 1, message = "Campo obrigatório. Por favor, digite um valor maior ou igual a 1.00")
     private double valor;
-    @NotNull (message = "Campo obrigatório. Por favor, digite o tipo de conta")
+    //@NotNull (message = "Campo obrigatório. Por favor, digite o tipo de conta")
     private Tipo tipo;
     @NotNull(message = "Campo obrigatório. Por favor, digite a data de vencimento")
     private LocalDate dataDeVencimento;
