36717644c258924210f0ea46c1d2fe41a47412a5
Arê Nascimento <aretuza.gab@gmail.com>
2021-11-12 00:35:09 -0300

    Inserindo anotaçõesde validações na contaDTO

--- src/main/java/br/com/zup/GerenciadorContas/dtos/ContaDTO.java
+++ src/main/java/br/com/zup/GerenciadorContas/dtos/ContaDTO.java
@@ -5,14 +5,23 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ContaDTO {
+    @NotNull
+    @Size(min = 2, max = 20, message = "Campo obrigatório. Por favor, digite um nome entre 2 a 20 caracteres")
     private String nome;
+    @NotNull(message = "Campo obrigatório. Por favor, digite o valor da conta")
     private double valor;
+    @NotNull (message = "Campo obrigatório. Por favor, digite o tipo de conta")
+    @Min(value = 0, message = "Digite um valor válido")
     private Tipo tipo;
+    @NotNull(message = "Campo obrigatório. Por favor, digite a data de vencimento")
     private LocalDate dataDeVencimento;
 }
